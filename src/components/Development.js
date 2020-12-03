59da500da6e01d2b0035d22c22ee284e14800cfc
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-12-02 20:12:23 -0500

    [UPLOAD]

--- src/components/Development.js
+++ src/components/Development.js
@@ -1,8 +1,30 @@
 import React from "react";
+import styles from "styled-components";
+
+const WrapperMenuView = styles.div`
+display: flex;`;
+
+const Cuadros = "./images/menucuadrado.png";
+const ImgCuadros = styles.img`
+width: 20px;
+height: 20px;
+margin-right: 10px;
+`;
+
+const Barras = "./images/menulateral.png";
+const ImgBarras = styles.img`
+width: 18px;
+height: 18px;
+`;
 
 class Development extends React.Component {
 	render() {
-		return <h1>HOLA</h1>;
+		return (
+			<WrapperMenuView>
+				<ImgCuadros src={Cuadros} />
+				<ImgBarras src={Barras} />
+			</WrapperMenuView>
+		);
 	}
 }
 
